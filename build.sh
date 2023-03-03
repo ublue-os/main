15f17ee7b779b5331e99a08701b629f53906c050
joshua-stone <joshua.gage.stone@gmail.com>
2023-03-02 22:52:54 -0500

    feat: add distrobox, just, and gnome-tweaks (#17)

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@
 set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
-echo $FEDORA_MAJOR_VERSION $IMAGE_NAME
 
 INCLUDED_PACKAGES=($(jq -r "[(.all.include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
                              (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
