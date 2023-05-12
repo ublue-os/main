235e3dc123f3489df05038f03000119cd388df1d
Benjamin Sherman <bsherman@users.noreply.github.com>
2023-05-12 16:09:02 -0500

    refactor: use new standard path for update-services (#199)

--- post-install.sh
+++ post-install.sh
@@ -7,4 +7,4 @@ systemctl enable flatpak-system-update.timer
 
 systemctl --global enable flatpak-user-update.timer
 
-cp /usr/share/ublue-os/ublue-os-update-services/etc/rpm-ostreed.conf /etc/rpm-ostreed.conf
+cp /usr/share/ublue-os/update-services/etc/rpm-ostreed.conf /etc/rpm-ostreed.conf
