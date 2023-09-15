0300c144cf227bed6262845c1bcaeb1074f756a3
RJ Trujillo <eyecantcu@pm.me>
2023-09-15 16:20:29 -0600

    fix(nvidia): Install 32 bit driver libraries
    Needed for Steam games and Wine

--- nvidia-install.sh
+++ nvidia-install.sh
@@ -19,5 +19,6 @@ fi
 
 rpm-ostree install \
     xorg-x11-drv-${NVIDIA_PACKAGE_NAME}-{,cuda-,devel-,kmodsrc-,power-}${NVIDIA_FULL_VERSION} \
+    xorg-x11-drv-${NVIDIA_PACKAGE_NAME}-libs-{,cuda-}${NVIDIA_FULL_VERSION}.i686 \
     nvidia-container-toolkit nvidia-vaapi-driver supergfxctl ${VARIANT_PKGS} \
     /tmp/akmods-rpms/kmods/kmod-${NVIDIA_PACKAGE_NAME}-${KERNEL_VERSION}-${NVIDIA_AKMOD_VERSION}.fc${RELEASE}.rpm
