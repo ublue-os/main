b2f58c5f3aa72c61b34638995299589a7bdf52d9
Benjamin Sherman <benjamin@holyarmy.org>
2023-09-22 14:37:31 -0500

    feat: reorganize to build nokmods then main and not nvidia

--- Containerfile
+++ Containerfile
@@ -3,44 +3,40 @@ ARG SOURCE_IMAGE="${SOURCE_IMAGE:-silverblue}"
 ARG BASE_IMAGE="quay.io/fedora-ostree-desktops/${SOURCE_IMAGE}"
 ARG FEDORA_MAJOR_VERSION="${FEDORA_MAJOR_VERSION:-38}"
 
+FROM ${BASE_IMAGE}:${FEDORA_MAJOR_VERSION} AS nokmods
 
-FROM ${BASE_IMAGE}:${FEDORA_MAJOR_VERSION} AS main
 ARG IMAGE_NAME="${IMAGE_NAME:-silverblue}"
 ARG FEDORA_MAJOR_VERSION="${FEDORA_MAJOR_VERSION:-38}"
 
 COPY github-release-install.sh /tmp/github-release-install.sh
-COPY main-install.sh /tmp/main-install.sh
-COPY main-post-install.sh /tmp/main-post-install.sh
-COPY main-packages.json /tmp/main-packages.json
+COPY nokmods-install.sh /tmp/nokmods-install.sh
+COPY nokmods-post-install.sh /tmp/nokmods-post-install.sh
+COPY nokmods-packages.json /tmp/nokmods-packages.json
 
 COPY --from=ghcr.io/ublue-os/config:latest /rpms /tmp/rpms
-COPY --from=ghcr.io/ublue-os/akmods:${FEDORA_MAJOR_VERSION} /rpms /tmp/akmods-rpms
 
-COPY system_files /
+RUN /tmp/nokmods-install.sh && \
+    /tmp/nokmods-post-install.sh && \
+    # temporary fix for https://github.com/containers/podman/issues/19930
+    rpm-ostree override replace https://bodhi.fedoraproject.org/updates/FEDORA-2023-8d641964bc && \
+    rm -rf /tmp/* /var/*
 
-RUN /tmp/main-install.sh
-RUN /tmp/main-post-install.sh
+RUN ostree container commit && \
+    mkdir -p /var/tmp && chmod -R 1777 /var/tmp
 
-# temporary fix for https://github.com/containers/podman/issues/19930
-RUN rpm-ostree override replace https://bodhi.fedoraproject.org/updates/FEDORA-2023-8d641964bc
+FROM nokmods AS main
 
-RUN rm -rf /tmp/* /var/*
-RUN ostree container commit
-RUN mkdir -p /var/tmp && chmod -R 1777 /var/tmp
-
-
-FROM main AS nvidia
 ARG IMAGE_NAME="${IMAGE_NAME:-silverblue}"
 ARG FEDORA_MAJOR_VERSION="${FEDORA_MAJOR_VERSION:-38}"
-ARG NVIDIA_MAJOR_VERSION="${NVIDIA_MAJOR_VERSION:-535}"
 
-COPY nvidia-install.sh /tmp/nvidia-install.sh
-COPY nvidia-post-install.sh /tmp/nvidia-post-install.sh
+COPY main-install.sh /tmp/main-install.sh
 
-COPY --from=ghcr.io/ublue-os/akmods-nvidia:${FEDORA_MAJOR_VERSION}-${NVIDIA_MAJOR_VERSION} /rpms /tmp/akmods-rpms
+COPY --from=ghcr.io/ublue-os/akmods:${FEDORA_MAJOR_VERSION} /rpms /tmp/akmods-rpms
 
-RUN /tmp/nvidia-install.sh
-RUN /tmp/nvidia-post-install.sh
-RUN rm -rf /tmp/* /var/*
-RUN ostree container commit
-RUN mkdir -p /var/tmp && chmod -R 1777 /tmp /var/tmp
+COPY main-sys_files /
+
+RUN /tmp/main-install.sh && \
+    rm -rf /tmp/* /var/*
+
+RUN ostree container commit && \
+    mkdir -p /var/tmp && chmod -R 1777 /var/tmp
--- main-install.sh
+++ main-install.sh
@@ -2,23 +2,11 @@
 
 set -ouex pipefail
 
-RELEASE="$(rpm -E %fedora)"
-
-INCLUDED_PACKAGES=($(jq -r "[(.all.include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
-                             (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
-                             | sort | unique[]" /tmp/main-packages.json))
-EXCLUDED_PACKAGES=($(jq -r "[(.all.exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
-                             (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
-                             | sort | unique[]" /tmp/main-packages.json))
-
-
-if [[ "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
-    EXCLUDED_PACKAGES=($(rpm -qa --queryformat='%{NAME} ' ${EXCLUDED_PACKAGES[@]}))
-fi
-
-wget -P /tmp/rpms \
-    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
-    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm
+rpm-ostree install /tmp/akmods-rpms/ublue-os/ublue-os-akmods-addons*.rpm
+for REPO in $(rpm -ql ublue-os-akmods-addons|grep ^"/etc"|grep repo$); do
+    echo "akmods: enable default entry: ${REPO}"
+    sed -i '0,/enabled=0/{s/enabled=0/enabled=1/}' ${REPO}
+done
 
 # force use of single rpmfusion mirror
 sed -i.bak 's%^metalink=%#metalink=%' /etc/yum.repos.d/rpmfusion-*.repo
@@ -28,19 +16,11 @@ if [[ "${FEDORA_MAJOR_VERSION}" -ge 39 ]]; then
     sed -i 's%free/fedora/releases%free/fedora/development%' /etc/yum.repos.d/rpmfusion-*.repo
 fi
 
-rpm-ostree install \
-    /tmp/rpms/*.rpm \
-    fedora-repos-archive
-
-rpm-ostree install /tmp/akmods-rpms/ublue-os/ublue-os-akmods-addons*.rpm
-for REPO in $(rpm -ql ublue-os-akmods-addons|grep ^"/etc"|grep repo$); do
-    echo "akmods: enable default entry: ${REPO}"
-    sed -i '0,/enabled=0/{s/enabled=0/enabled=1/}' ${REPO}
-done
-
 # Only run if FEDORA_MAJOR_VERSION is not 39
 if grep -qv "39" <<< $FEDORA_MAJOR_VERSION; then
     rpm-ostree install \
+        kernel-devel-matched \
+        kernel-tools \
         /tmp/akmods-rpms/kmods/*xpadneo*.rpm \
         /tmp/akmods-rpms/kmods/*xpad-noone*.rpm \
         /tmp/akmods-rpms/kmods/*xone*.rpm \
@@ -53,27 +33,6 @@ for REPO in $(rpm -ql ublue-os-akmods-addons|grep ^"/etc"|grep repo$); do
     echo "akmods: disable per defaults: ${REPO}"
     sed -i 's@enabled=1@enabled=0@g' ${REPO}
 done
-
-if [[ "${#INCLUDED_PACKAGES[@]}" -gt 0 && "${#EXCLUDED_PACKAGES[@]}" -eq 0 ]]; then
-    rpm-ostree install \
-        ${INCLUDED_PACKAGES[@]}
-
-elif [[ "${#INCLUDED_PACKAGES[@]}" -eq 0 && "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
-    rpm-ostree override remove \
-        ${EXCLUDED_PACKAGES[@]}
-
-elif [[ "${#INCLUDED_PACKAGES[@]}" -gt 0 && "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
-    rpm-ostree override remove \
-        ${EXCLUDED_PACKAGES[@]} \
-        $(printf -- "--install=%s " ${INCLUDED_PACKAGES[@]})
-
-else
-    echo "No packages to install."
-
-fi
-
-## install packages direct from github
-/tmp/github-release-install.sh sigstore/cosign x86_64
 
 # reset forced use of single rpmfusion mirror
 rename -v .repo.bak .repo /etc/yum.repos.d/rpmfusion-*repo.bak
