bfd2156ea423ee61c9c3f183986df988b6288a57
Renner0E <80410025+Renner0E@users.noreply.github.com>
2023-10-28 10:50:08 +0200

    move vim neovim symlink to post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -9,4 +9,7 @@ systemctl --global enable flatpak-user-update.timer
 
 cp /usr/share/ublue-os/update-services/etc/rpm-ostreed.conf /etc/rpm-ostreed.conf
 
-ln -s "/usr/share/fonts/google-noto-sans-cjk-fonts" "/usr/share/fonts/noto-cjk" 
+ln -s "/usr/share/fonts/google-noto-sans-cjk-fonts" "/usr/share/fonts/noto-cjk"
+
+# vim neovim symlink
+ln -s /usr/bin/nvim /usr/bin/vim
