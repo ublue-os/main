cdc36c6c02ed8916b95c0e17ffa72799c185cc6d
Benjamin Sherman <benjamin@holyarmy.org>
2023-11-06 10:41:37 -0600

    chore: restore default rpmfusion mirror behavior (#413)

--- install.sh
+++ install.sh
@@ -12,19 +12,8 @@ rpm-ostree install \
     /tmp/rpms/*.rpm \
     fedora-repos-archive
 
-# force use of single rpmfusion mirror
-sed -i.bak 's%^metalink=%#metalink=%' /etc/yum.repos.d/rpmfusion-*.repo
-sed -i 's%^#baseurl=http://download1.rpmfusion.org%baseurl=http://mirrors.ocf.berkeley.edu/rpmfusion%' /etc/yum.repos.d/rpmfusion-*.repo
-# after F39 launches, bump to 40
-if [[ "${FEDORA_MAJOR_VERSION}" -ge 39 ]]; then
-    sed -i 's%free/fedora/releases%free/fedora/development%' /etc/yum.repos.d/rpmfusion-*.repo
-fi
-
 # run common packages script
 /tmp/packages.sh
 
 ## install packages direct from github
 /tmp/github-release-install.sh sigstore/cosign x86_64
-
-# reset forced use of single rpmfusion mirror
-rename -v .repo.bak .repo /etc/yum.repos.d/rpmfusion-*repo.bak
