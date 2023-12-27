69aabfc5298c560fb1c81fb3fe992f3dd8fa353d
Benjamin Sherman <benjamin@holyarmy.org>
2023-12-26 21:33:41 -0600

    chore(ci): pin rpmfusion repo install too

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 wget -P /tmp/rpms \
-    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
-    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm
+    http://mirrors.ocf.berkeley.edu/rpmfusion/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
+    http://mirrors.ocf.berkeley.edu/rpmfusion/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm
 
 rpm-ostree install \
     /tmp/rpms/*.rpm \
