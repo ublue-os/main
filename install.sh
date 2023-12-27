8f0d032b46114219e02f4779c7facd0f11a50038
Benjamin Sherman <benjamin@holyarmy.org>
2023-12-26 21:22:23 -0600

    chore(ci): pin to single berkley mirror
    This consistently pins the build to the same mirror so jobs running on different runners should have the same set of packages to consistently succeed/fail.

--- install.sh
+++ install.sh
@@ -12,8 +12,19 @@ rpm-ostree install \
     /tmp/rpms/*.rpm \
     fedora-repos-archive
 
+# force use of single rpmfusion mirror
+sed -i.bak 's%^metalink=%#metalink=%' /etc/yum.repos.d/rpmfusion-*.repo
+sed -i 's%^#baseurl=http://download1.rpmfusion.org%baseurl=http://mirrors.ocf.berkeley.edu/rpmfusion%' /etc/yum.repos.d/rpmfusion-*.repo
+# after F40 launches, bump to 41
+if [[ "${FEDORA_MAJOR_VERSION}" -ge 40 ]]; then
+    sed -i 's%free/fedora/releases%free/fedora/development%' /etc/yum.repos.d/rpmfusion-*.repo
+fi
+
 # run common packages script
 /tmp/packages.sh
 
 ## install packages direct from github
 /tmp/github-release-install.sh sigstore/cosign x86_64
+
+# reset forced use of single rpmfusion mirror
+rename -v .repo.bak .repo /etc/yum.repos.d/rpmfusion-*repo.bak
