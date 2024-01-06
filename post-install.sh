902f17eee4ab20d2e6f9056e0dcb997860714bb9
joshua-stone <joshua.gage.stone@gmail.com>
2024-01-06 17:11:56 -0500

    Strip out unneeded components and build targets

--- post-install.sh
+++ post-install.sh
@@ -2,11 +2,4 @@
 
 set -ouex pipefail
 
-systemctl enable rpm-ostreed-automatic.timer
-systemctl enable flatpak-system-update.timer
-
-systemctl --global enable flatpak-user-update.timer
-
-cp /usr/share/ublue-os/update-services/etc/rpm-ostreed.conf /etc/rpm-ostreed.conf
-
 ln -s "/usr/share/fonts/google-noto-sans-cjk-fonts" "/usr/share/fonts/noto-cjk" 
