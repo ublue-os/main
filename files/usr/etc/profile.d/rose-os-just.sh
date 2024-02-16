9c1d70dfe4448fe493d4065409aa15ed0c1a2ac7
joshua-stone <joshua.gage.stone@gmail.com>
2024-02-16 14:55:00 -0500

    Fix up packages and justfile

--- files/usr/etc/profile.d/rose-os-just.sh
+++ files/usr/etc/profile.d/rose-os-just.sh
@@ -1,5 +1,8 @@
 if [ ! -z "$HOME" ] && [ -d "$HOME" ] && [ ! -f "${HOME}/.justfile" ]; then
   cat > "${HOME}/.justfile" << EOF
 import "/usr/share/rose-os/justfile"
+
+default:
+    @just --list
 EOF
 fi
