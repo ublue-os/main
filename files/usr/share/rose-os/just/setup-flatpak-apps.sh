9280c08c728a5af087a03da1db977ac1c7bb5810
joshua-stone <joshua.gage.stone@gmail.com>
2024-02-16 14:05:58 -0500

    Add justfile integration

---
+++ files/usr/share/rose-os/just/setup-flatpak-apps.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -uoex pipefail
+
+readonly SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+readonly FLATPAK_FILE="$(dirname "${SCRIPT_DIR}")/flatpak-apps.txt"
+FLATPAK_APPS=$(<"${FLATPAK_FILE}")
+
+readonly REMOTES=($(flatpak remotes --columns=name))
+
+for REMOTE in "${REMOTES[@]}"; do
+    if [[ "${REMOTE}" =~ fedora* ]]; then
+	echo "Deleting flatpak remote: ${REMOTE}"
+        flatpak remote-delete "${REMOTE}"
+    fi
+done
+
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+flatpak install --noninteractive ${FLATPAK_APPS[@]}
