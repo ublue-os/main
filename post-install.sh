6ed8fa68d1c03c3c072fc9247ba1c2b8949bee57
joshua-stone <joshua.gage.stone@gmail.com>
2024-03-06 14:41:07 -0500

    Regenerate fontconfig cache

--- post-install.sh
+++ post-install.sh
@@ -4,5 +4,7 @@ set -ouex pipefail
 
 ln -s "/usr/share/fonts/google-noto-sans-cjk-fonts" "/usr/share/fonts/noto-cjk"
 
+fc-cache --system-only --really-force --verbose
+
 sed -i "s@enabled=1@enabled=0@g" /etc/yum.repos.d/fedora-cisco-openh264.repo \
                                  /etc/yum.repos.d/rpmfusion-*.repo
