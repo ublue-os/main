c9e95f4f0a5434d47689fb5efdbc84672e235cdb
Benjamin Sherman <benjamin@holyarmy.org>
2024-10-04 00:31:59 -0500

    properly swap mesa and some libva drivers to negativo17

--- install.sh
+++ install.sh
@@ -35,10 +35,28 @@ fi
 # use negativo17 for 3rd party packages with higher priority than default
 curl -Lo /etc/yum.repos.d/negativo17-fedora-multimedia.repo https://negativo17.org/repos/fedora-multimedia.repo
 sed -i '0,/enabled=0/{s/enabled=0/enabled=1\npriority=90/}' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
-cat /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 
 # use override to replace mesa drivers with the less crippled version
-rpm-ostree override replace --experimental --from repo='fedora-multimedia' mesa-dri-drivers mesa-filesystem mesa-libglapi mesa-va-drivers
+rpm-ostree override replace \
+  --experimental \
+  --from repo='fedora-multimedia' \
+    mesa-dri-drivers \
+    mesa-filesystem \
+    mesa-libEGL \
+    mesa-libGL \
+    mesa-libgbm \
+    mesa-libglapi \
+    mesa-libxatracker \
+    mesa-va-drivers \
+    mesa-vulkan-drivers
+
+# use override to replace other video accleration pacakges
+rpm-ostree override replace \
+  --experimental \
+  --from repo='fedora-multimedia' \
+    libva \
+    libva-intel-media-driver \
+    libvdpau
 
 # run common packages script
 /ctx/packages.sh
