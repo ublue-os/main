1114c7cd2e766f1dc57b9ca4085f0cdae67e1b99
Benjamin Sherman <benjamin@holyarmy.org>
2024-10-26 14:47:59 -0500

    fix: handle new F41 selinux policies for sulogin-generator
    Remove our in-repo copy of `ublue-sulogin-generator` and replace with
    the current version from CoreOS. This way we will stay in sync with that
    as an upstream, and comply with new Fedora 41 SElinux policy.
    
    Safe for F39/F40 as well.
    
    Fixes: #653

--- install.sh
+++ install.sh
@@ -66,6 +66,12 @@ fi
 ## install packages direct from github
 /ctx/github-release-install.sh sigstore/cosign x86_64
 
+# use CoreOS' generator for emergency/rescue boot
+# see detail: https://github.com/ublue-os/main/issues/653
+CSFG=/usr/lib/systemd/system-generators/coreos-sulogin-force-generator
+curl -sSLo ${CSFG} https://raw.githubusercontent.com/coreos/fedora-coreos-config/refs/heads/stable/overlay.d/05core/usr/lib/systemd/system-generators/coreos-sulogin-force-generator
+chmod +x ${CSFG}
+
 if [[ "${KERNEL_VERSION}" == "${QUALIFIED_KERNEL}" ]]; then
     /ctx/initramfs.sh
 fi
