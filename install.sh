1c713f993f0cdf253dd96114f86f5354425c96c0
Kyle Gospodnetich <me@kylegospodneti.ch>
2024-11-02 11:25:31 -0700

    feat: Replace gnome-software with non-dkms version on F41 and above.

--- install.sh
+++ install.sh
@@ -60,6 +60,14 @@ if [[ "$FEDORA_MAJOR_VERSION" -ne "41" ]]; then
         libvdpau
 fi
 
+# Disable DKMS support in gnome-software
+if [[ "$FEDORA_MAJOR_VERSION" -ge "41" && "$IMAGE_NAME" == "silverblue" ]]; then
+    rpm-ostree override replace \
+        --experimental \
+        --from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
+        gnome-software
+fi
+
 # run common packages script
 /ctx/packages.sh
 
