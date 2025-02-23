ced83e449872d96094c17298161938aac9c87d33
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-02-22 22:12:32 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -44,27 +44,42 @@ fi
 curl -Lo /etc/yum.repos.d/negativo17-fedora-multimedia.repo https://negativo17.org/repos/fedora-multimedia.repo
 sed -i '0,/enabled=1/{s/enabled=1/enabled=1\npriority=90/}' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 
-# use override to replace mesa and others with less crippled versions
-rpm-ostree override replace \
-  --experimental \
-  --from repo='fedora-multimedia' \  
-  --remove=mesa-libglapi \
-    libheif \
-    libva \
-    libva-intel-media-driver \
-    mesa-dri-drivers \
-    mesa-filesystem \
-    mesa-libEGL \
-    mesa-libGL \
-    mesa-libgbm \
-    mesa-libxatracker \
-    mesa-va-drivers \
-    mesa-vulkan-drivers
+if [[ "$FEDORA_MAJOR_VERSION" -ne "40" ]]; then
+    # use override to replace mesa and others with less crippled versions
+    rpm-ostree override replace \
+      --experimental \
+      --from repo='fedora-multimedia' \  
+        libheif \
+        libva \
+        libva-intel-media-driver \
+        mesa-dri-drivers \
+        mesa-filesystem \
+        mesa-libEGL \
+        mesa-libglapi \
+        mesa-libGL \
+        mesa-libgbm \
+        mesa-libxatracker \
+        mesa-va-drivers \
+        mesa-vulkan-drivers
+fi
 
 if [[ "$FEDORA_MAJOR_VERSION" -ne "41" ]]; then
+    # use override to replace mesa and others with less crippled versions
     rpm-ostree override replace \
-        --experimental \
-        --from repo='fedora-multimedia' \
+      --experimental \
+      --from repo='fedora-multimedia' \  
+      --remove=mesa-libglapi \
+        libheif \
+        libva \
+        libva-intel-media-driver \
+        mesa-dri-drivers \
+        mesa-filesystem \
+        mesa-libEGL \
+        mesa-libGL \
+        mesa-libgbm \
+        mesa-libxatracker \
+        mesa-va-drivers \
+        mesa-vulkan-drivers \
         libvdpau
 fi
 
