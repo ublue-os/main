f01bbaac698286f7e673e6edd3a2b9e55bdc7f8e
m2 <69128853+m2Giles@users.noreply.github.com>
2025-03-22 17:19:14 -0400

    chore: cleanup packages.sh
    packages.sh and packages.json are designed around the behavior of
    rpm-ostree. Namely, it can install multiple packages while removing
    multiple packages in a single transaction. `dnf5` does not support this.
    `dnf5 distro-sync` can bring multiple packages into sync at once, but
    cannot be used to install packages. `dnf5 swap` only works on one
    package at a time.
    
    This cleanup removes the behaviour of doing a single transaction of
    installing and removing. Instead, it parses the the json file to install
    packages first. Then, it parses the json file and confirms the packages
    are on image before removing them.
    
    Already merged is a basic package checker for some of the major packages
    for each image (F42 images only. Lazurite and Vauxite are on their own
    right now.)
    
    This is a short term change right now that slightly changes
    functionality. We already are installing items in install.sh seperate
    from this json being the ultimate source of truth.
    
    Signed-off-by: m2 <69128853+m2Giles@users.noreply.github.com>

--- build_files/packages.sh
+++ build_files/packages.sh
@@ -1,53 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 
 set -ouex pipefail
 
-RELEASE="$(rpm -E %fedora)"
-
 # build list of all packages requested for inclusion
-INCLUDED_PACKAGES=($(jq -r "[(.all.include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
+readarray -t INCLUDED_PACKAGES < <(jq -r "[(.all.include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
                              (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".include | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
-                             | sort | unique[]" /ctx/packages.json))
+                             | sort | unique[]" /ctx/packages.json)
 
-# build list of all packages requested for exclusion
-EXCLUDED_PACKAGES=($(jq -r "[(.all.exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
-                             (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
-                             | sort | unique[]" /ctx/packages.json))
-
-
-# ensure exclusion list only contains packages already present on image
-if [[ "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
-    EXCLUDED_PACKAGES=($(rpm -qa --queryformat='%{NAME} ' ${EXCLUDED_PACKAGES[@]}))
-fi
-
-# simple case to install where no packages need excluding
-if [[ "${#INCLUDED_PACKAGES[@]}" -gt 0 && "${#EXCLUDED_PACKAGES[@]}" -eq 0 ]]; then
+# Install Packages
+if [[ "${#INCLUDED_PACKAGES[@]}" -gt 0 ]]; then
     dnf5 -y install \
-        ${INCLUDED_PACKAGES[@]}
-
-# install/excluded packages both at same time
-elif [[ "${#INCLUDED_PACKAGES[@]}" -gt 0 && "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
-    dnf5 -y remove \
-        ${EXCLUDED_PACKAGES[@]}
-    dnf5 -y install \
-        ${INCLUDED_PACKAGES[@]}
+        "${INCLUDED_PACKAGES[@]}"
 else
     echo "No packages to install."
 
 fi
 
-# check if any excluded packages are still present
-# (this can happen if an included package pulls in a dependency)
-EXCLUDED_PACKAGES=($(jq -r "[(.all.exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
+# build list of all packages requested for exclusion
+readarray -t EXCLUDED_PACKAGES < <(jq -r "[(.all.exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[]), \
                              (select(.\"$FEDORA_MAJOR_VERSION\" != null).\"$FEDORA_MAJOR_VERSION\".exclude | (.all, select(.\"$IMAGE_NAME\" != null).\"$IMAGE_NAME\")[])] \
-                             | sort | unique[]" /ctx/packages.json))
+                             | sort | unique[]" /ctx/packages.json)
 
 if [[ "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
-    EXCLUDED_PACKAGES=($(rpm -qa --queryformat='%{NAME} ' ${EXCLUDED_PACKAGES[@]}))
+    readarray -t EXCLUDED_PACKAGES < <(rpm -qa --queryformat='%{NAME}\n' "${EXCLUDED_PACKAGES[@]}")
 fi
 
 # remove any excluded packages which are still present on image
 if [[ "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
     dnf5 -y remove \
-        ${EXCLUDED_PACKAGES[@]}
+        "${EXCLUDED_PACKAGES[@]}"
+else
+    echo "No packages to remove."
 fi
