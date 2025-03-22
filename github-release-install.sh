bfa11636ad3d55c4f848bdb894567c6c7452ac0e
Robert Sturla <robertsturla@outlook.com>
2025-03-22 08:44:06 +0000

    Revert "chore: enable F42 images" (#764)

--- github-release-install.sh
+++ github-release-install.sh
@@ -11,6 +11,7 @@
 # example: wezterm builds RPMs for different distros so we must be more specific.
 #   ARCH_FILTER of "fedora37.x86_64" gets the x86_64 RPM build for fedora37
 
+
 ORG_PROJ=${1}
 ARCH_FILTER=${2}
 LATEST=${3}
@@ -46,11 +47,11 @@ API="https://api.github.com/repos/${ORG_PROJ}/releases/${RELTAG}"
 
 # retry up to 5 times with 5 second delays for any error included HTTP 404 etc
 curl --fail --retry 5 --retry-delay 5 --retry-all-errors -sL ${API} -o ${API_JSON}
-RPM_URLS=($(cat ${API_JSON} |
-  jq \
+RPM_URLS=($(cat ${API_JSON} \
+  | jq \
     -r \
     --arg arch_filter "${ARCH_FILTER}" \
     '.assets | sort_by(.created_at) | reverse | .[] | select(.name|test($arch_filter)) | select(.name|test("rpm$")) | .browser_download_url'))
 # WARNING: in case of multiple matches, this only installs the first matched release
-echo "execute: dnf5 -y install \"${RPM_URLS[0]}\""
-dnf5 -y install "${RPM_URLS[0]}"
+echo "execute: rpm-ostree install \"${RPM_URLS[0]}\""
+rpm-ostree install "${RPM_URLS[0]}"
---
+++ install.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/bash
+
+set -ouex pipefail
+
+RELEASE="$(rpm -E %fedora)"
+KERNEL_SUFFIX=""
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
+
+# mitigate upstream packaging bug: https://bugzilla.redhat.com/show_bug.cgi?id=2332429
+# swap the incorrectly installed OpenCL-ICD-Loader for ocl-icd, the expected package
+rpm-ostree override replace \
+  --from repo='fedora' \
+  --experimental \
+  --remove=OpenCL-ICD-Loader \
+  ocl-icd \
+  || true
+
+curl -Lo /etc/yum.repos.d/_copr_ublue-os_packages.repo https://copr.fedorainfracloud.org/coprs/ublue-os/packages/repo/fedora-"${RELEASE}"/ublue-os-packages-fedora-"${RELEASE}".repo
+curl -Lo /etc/yum.repos.d/_copr_ublue-os_staging.repo https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-"${RELEASE}"/ublue-os-staging-fedora-"${RELEASE}".repo
+curl -Lo /etc/yum.repos.d/_copr_kylegospo_oversteer.repo https://copr.fedorainfracloud.org/coprs/kylegospo/oversteer/repo/fedora-"${RELEASE}"/kylegospo-oversteer-fedora-"${RELEASE}".repo
+
+rpm-ostree install \
+    ublue-os-just \
+    ublue-os-luks \
+    ublue-os-signing \
+    ublue-os-udev-rules \
+    ublue-os-update-services \
+    /tmp/akmods-rpms/*.rpm \
+    fedora-repos-archive
+
+mv /usr/etc/containers/policy.json /etc/containers/policy.json
+
+# Handle Kernel Skew with override replace
+if [[ "${KERNEL_VERSION}" == "${QUALIFIED_KERNEL}" ]]; then
+    echo "Installing signed kernel from kernel-cache."
+    cd /tmp
+    rpm2cpio /tmp/kernel-rpms/kernel-core-*.rpm | cpio -idmv
+    cp ./lib/modules/*/vmlinuz /usr/lib/modules/*/vmlinuz
+    cd /
+else
+    echo "Install kernel version ${KERNEL_VERSION} from kernel-cache."
+    rpm-ostree override replace \
+        --experimental \
+        --install=zstd \
+        /tmp/kernel-rpms/kernel-[0-9]*.rpm \
+        /tmp/kernel-rpms/kernel-core-*.rpm \
+        /tmp/kernel-rpms/kernel-modules-*.rpm
+fi
+
+# use negativo17 for 3rd party packages with higher priority than default
+curl -Lo /etc/yum.repos.d/negativo17-fedora-multimedia.repo https://negativo17.org/repos/fedora-multimedia.repo
+sed -i '0,/enabled=1/{s/enabled=1/enabled=1\npriority=90/}' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+
+if [[ "$FEDORA_MAJOR_VERSION" -le "40" ]]; then
+    # use override to replace mesa and others with less crippled versions
+    rpm-ostree override replace \
+      --experimental \
+      --from repo='fedora-multimedia' \
+        libheif \
+        libva \
+        libva-intel-media-driver \
+        mesa-dri-drivers \
+        mesa-filesystem \
+        mesa-libEGL \
+        mesa-libglapi \
+        mesa-libGL \
+        mesa-libgbm \
+        mesa-libxatracker \
+        mesa-va-drivers \
+        mesa-vulkan-drivers \
+        libvdpau
+fi
+
+if [[ "$FEDORA_MAJOR_VERSION" -ge "41" ]]; then
+    # use override to replace mesa and others with less crippled versions
+    rpm-ostree override replace \
+      --experimental \
+      --from repo='fedora-multimedia' \
+        libheif \
+        libva \
+        libva-intel-media-driver \
+        mesa-dri-drivers \
+        mesa-filesystem \
+        mesa-libEGL \
+        mesa-libGL \
+        mesa-libgbm \
+        mesa-libxatracker \
+        mesa-va-drivers \
+        mesa-vulkan-drivers
+fi
+
+# Disable DKMS support in gnome-software
+if [[ "$FEDORA_MAJOR_VERSION" -ge "41" && "$IMAGE_NAME" == "silverblue" ]]; then
+    rpm-ostree override remove \
+        gnome-software-rpm-ostree
+    rpm-ostree override replace \
+        --experimental \
+        --from repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
+        gnome-software
+fi
+
+# run common packages script
+/ctx/packages.sh
+
+## install packages direct from github
+/ctx/github-release-install.sh sigstore/cosign x86_64
+
+# use CoreOS' generator for emergency/rescue boot
+# see detail: https://github.com/ublue-os/main/issues/653
+CSFG=/usr/lib/systemd/system-generators/coreos-sulogin-force-generator
+curl -sSLo ${CSFG} https://raw.githubusercontent.com/coreos/fedora-coreos-config/refs/heads/stable/overlay.d/05core/usr/lib/systemd/system-generators/coreos-sulogin-force-generator
+chmod +x ${CSFG}
+
+if [[ "${KERNEL_VERSION}" == "${QUALIFIED_KERNEL}" ]]; then
+    /ctx/initramfs.sh
+fi
