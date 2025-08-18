9de53b484afb70d1eda25437596d6e0d3295298a
Kyle Gospodnetich <me@kylegospodneti.ch>
2025-08-18 10:18:20 -0700

    chore: Clean up comments

--- build_files/install.sh
+++ build_files/install.sh
@@ -96,9 +96,11 @@ dnf5 remove -y \
     fedora-flathub-remote \
     fedora-third-party
 
-# Add Flathub to the image for application, ensure Fedora's repository's service is overridden.
+# Add Flathub to the image for eventual application
 mkdir -p /etc/flatpak/remotes.d/
 curl --retry 3 -Lo /etc/flatpak/remotes.d/flathub.flatpakrepo https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# Fedora Flatpak service is a part of the flatpak package, ensure it's overridden by moving to replace it at the end of the build.
 mv -f /usr/lib/systemd/system/flatpak-add-flathub-repos.service /usr/lib/systemd/system/flatpak-add-fedora-repos.service
 
 # Prevent partial QT upgrades that may break SDDM/KWin
