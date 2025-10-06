cc930db46ec2fef0cbbcd0a93d46bb991c2ca419
Robert Sturla <robertsturla@outlook.com>
2025-10-06 22:57:44 +0100

    chore: update image-versions.yaml with latest digests, skip nvidia-container-toolkit in F43

--- build_files/nvidia-install.sh
+++ build_files/nvidia-install.sh
@@ -83,8 +83,14 @@ dnf5 install -y \
     nvidia-driver-cuda-libs.i686 \
     nvidia-driver-libs.i686 \
     nvidia-settings \
-    nvidia-container-toolkit ${VARIANT_PKGS} \
+    ${VARIANT_PKGS} \
     "${AKMODNV_PATH}"/kmods/kmod-nvidia-"${KERNEL_VERSION}"-"${NVIDIA_AKMOD_VERSION}"."${DIST_ARCH}".rpm
+
+# nvidia-container-toolkit is not built with required crypto digests for RPM
+# 6+, introduced in Fedora 43
+if [[ "${RELEASE}" -ne 43 ]]; then
+    dnf5 install -y nvidia-container-toolkit
+fi
 
 # Ensure the version of the Nvidia module matches the driver
 KMOD_VERSION="$(rpm -q --queryformat '%{VERSION}' kmod-nvidia)"
