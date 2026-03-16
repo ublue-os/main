7523b7339bc723774184160a37d3f6d59f3b2cd2
inffy <10782843+inffy@users.noreply.github.com>
2026-03-16 07:40:42 +0200

    chore: enable F44 builds (#2192)

--- build_files/nvidia-install.sh
+++ build_files/nvidia-install.sh
@@ -104,8 +104,6 @@ fi
 systemctl enable ublue-nvctk-cdi.service
 semodule --verbose --install /usr/share/selinux/packages/nvidia-container.pp
 
-# Universal Blue specific Initramfs fixes
-cp /etc/modprobe.d/nvidia-modeset.conf /usr/lib/modprobe.d/nvidia-modeset.conf
 # we must force driver load to fix black screen on boot for nvidia desktops
 sed -i 's@omit_drivers@force_drivers@g' /usr/lib/dracut/dracut.conf.d/99-nvidia.conf
 # as we need forced load, also mustpre-load intel/amd iGPU else chromium web browsers fail to use hardware acceleration
